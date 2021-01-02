71ccc4185a0861ecb81a1b01a22f1e2ea9e1fa15
aybjax <aybat93@gmail.com>
2021-01-03 00:14:36 +0600

    configuring php - failed

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,33 @@
 <?php
+// require_once '../project/init.php';
 
-$result = 1 + 2;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use MongoDB\MongoClient;
 
-echo "result of 1 + 2 = $result";
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/status', function (Request $request, Response $response, array $args) {
+    $m = new MongoClient('mongodb://mongo:27017');
+
+    echo "after <br/>";
+
+    $payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+
+});
+
+$app->post('/', function (Request $request, Response $response, array $args) {
+    $payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+
+});
+
+$app->run();
